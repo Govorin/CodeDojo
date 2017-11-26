4af45c1e8dfe948fbc1a66ef1332ea012807793e
govorin <govorinviktor@gmail.com>
2017-11-26 12:55:45 +0300

    Closure HW

--- closure.js
+++ closure.js
@@ -81,3 +81,82 @@ console.log(subtractReversed(3,4));
 const curry = fn => x => y => fn(x,y);
 const addCurry = curry(add);
 console.log(addCurry(3)(4));
+
+//1st HW
+function limit(fn,i) {
+  let count=0;
+  return function (...args) {
+    if (count<i){
+      count++;
+      return fn(...args);
+    }
+    else {
+      return false;
+    }
+  };
+}
+function playGame() {
+  console.log('Playing game =)');
+}
+
+let playGameLimited = limit(playGame, 3);
+
+playGameLimited();
+playGameLimited();
+playGameLimited();
+playGameLimited();
+
+//2nd HW
+
+function protect(value,password) {
+  return function (string) {
+    if(string===password){
+      return value;
+    }
+  };
+};
+let getSecret = protect('1234-5678-9012-3456', 'qwerty');
+getSecret('qwerty');
+getSecret();
+
+// 3rd HW
+function createCounter(start = 0, step = 1) {
+  return function () {
+    return start += step;
+  };
+}
+
+//4th
+function createList(array=[]) {
+	let items=array;
+	function getLength() {
+		return items.length;
+  }
+  function add(item) {
+		return items.push(item);
+  }
+  function get(index) {
+		return items[index];
+  }
+  function set(index,item) {
+		items[index]=item;
+  }
+  function remove(item) {
+		let index=items.indexOf(item);
+    if (index > -1) {
+      items.splice(index, 1);
+    }
+  }
+  function toString() {
+		return items.toString();
+  }
+  return {
+    items,
+    getLength,
+    add,
+    get,
+    set,
+    remove,
+    toString
+  };
+}
